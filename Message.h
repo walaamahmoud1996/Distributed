741959bae38ba742021f319b76e9657af080b690
ahmed531998 <37671912+ahmed531998@users.noreply.github.com>
2018-10-27 19:14:37 +0200

    Update Message.h

--- Message.h
+++ Message.h
@@ -1,32 +1,32 @@
 #ifndef MESSAGE_H
 #define MESSAGE_H
-enum MessageType { Request, Reply,Auth,StatusReply,ImageListReply};//needs to be updated
+enum MessageType {Request, Reply};
 class Message
 {
 private:
 	MessageType message_type;
 	int operation;
-	void * message;
+	string message;
 	size_t message_size;
 	int rpc_id;
 
-	
+
 public:
-	Message(int operation, void * p_message, size_t p_message_size,int p_rpc_id);
+	Message(int operation, string p_message, size_t p_message_size,int p_rpc_id);
 	Message(char * marshalled_base64);
-	char * marshal ();
+	string marshal ();
 	int getOperation ();
 	int getRPCId();
-	void * getMessage();
+	string getMessage();
 	size_t getMessageSize();
 	MessageType getMessageType();
 	void setOperation (int _operation);
-	void setMessage (void * message, size_t message_size);
+	void setMessage (string message, size_t message_size);
 	void setMessageType (MessageType message_type);
 
 	/*
 
-		RPC messages 
+		RPC messages
 
 	*/
 	~Message();
