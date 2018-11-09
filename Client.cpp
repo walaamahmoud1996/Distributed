d4130463b9c25f2c4960b11833fe80b4ffc23ced
walaaosman <walaa.mahmoud@aucegypt.edu>
2018-11-09 18:44:14 +0200

    -a

--- Client.cpp
+++ Client.cpp
@@ -2,6 +2,7 @@
 
 Client::Client(char * _hostname, int _port)
 {
+	rpc_id=0;
 	hostname = _hostname;
 	send_port = _port;
 
@@ -13,6 +14,9 @@ Client::Client(char * _hostname, int _port)
 		cout<<"Failed To intialize a Client\n";
 
 
+
+}
+Client::Client(){
 
 }
 
@@ -125,5 +129,195 @@ Message Client::execute(Message _message){
 
 	Message Returned(Recieved);
 	return Returned;
+}
+void Client::Upload_Image(string image_name){
+
+	Image newImage(image_name+".jpeg",image_name+".txt");
+
+}
+
+void Client::Login(string user_name,string password){
+	vector<string> login_p;//parameters
+	vector<string> login_r;//reply
+
+	login_p.push_back(user_name);
+	login_p.push_back(password);
+	rpc_id++;
+
+
+	Message login_message(int(login),Request,login_p,rpc_id);
+
+	Message login_reply = execute(login_message);
+
+	if(login_reply.getMessageType() == Reply){
+
+		login_r = login_reply.getMessage();
+		if(login_r[0]=="true"){
+
+			cout<<"YOU ARE LOGGED IN"<<endl;
+			this->username = user_name;
+			this->password = password;
+			this->Client_Token = login_r[1];
+			//this->Give_me_images();
+		}
+		else
+		{
+			cout<<"RELOGIN";
+		}
+	}
+	
+	
+
+}
+void Client::Ping(){
+
+	vector<string> ping_p;
+	ping_p.push_back(this->Client_Token);
+	ping_p.push_back(this->ip_address);
+	ping_p.push_back(to_string())
+	rpc_id++;
+	Message ping_request(int(ping),Request,ping_p,rpc_id);
+
+	Message ping_reply = execute(ping_request);
+	vector<string>images_lables = ping_reply.getMessage();
+
+	images_to_see.insert(images_to_see.end(),images_lables.begin(),images_lables.end())
+
+}
+void Client::Take_my_recources(string image_name){
+
+	Image myimg(image_name+".jpeg",image_name+".txt");
+	string image = myimg.Get_Actual_Image();
+	vector<string> take_recource_p;
+	vector<string> take_recource_r;
+
+	take_recource_p.push_back(this->username);
+
+	take_recource_p.push_back(image_name);
+	take_recource_p.push_back(image);
+
+	rpc_id++;
+	Message take_recource(int(Take_my_recource),Request,take_recource_p,rpc_id);
+
+	Message take_recource_reply = execute(take_recource);
+
+	if(take_recource_reply.getMessageType()==Reply){
+
+		take_recource_r = take_recource_reply.getMessage();
+
+		if(take_recource_r[0]=="true"){
+
+			cout<<"IMAGE AT DIRECTORY NOW";
+		}
+		else 
+		{
+			cout<<"IMGAE IS NOT THERE ";
+		}
+	}
+
+}
+
+void Client::Give_me_images(){
+
+	vector<string> Images_to_see;
+
+	vector<string> give_me_p;
+	give_me_p.push_back(this->username);
+
+	rpc_id++;
+	Message give_me_request(int(give_me_images),Request,give_me_p,rpc_id);
+
+	Message give_me_reply = execute(give_me_request);
+
+
+	if(give_me_reply.getMessageType()==Reply){
+
+		Images_to_see = give_me_reply.getMessage();
+
+		for(int i=0;i<Images_to_see.size();i+=2)
+		{	
+			
+			name_to_image[Images_to_see[i]] = Images_to_see[i+1];
+
+			Image cover_img(Images_to_see[i],Images_to_see[i+1],0);
+			Image actual_img(Images_to_see[i],"",1);
+
+			ProcessText(Images_to_see[i]);
+
+
+		}
+
+
+
+
+	}
+}
+void Client::ProcessText(string text_name){
+
+	string file = text_name+"txt";
+	ifstream image_text(file.c_str(),ios::binary);
+
+
+ 	string user;
+ 	string qouta;
+
+ 	while(!image_text.eof())
+ 	{
+ 		image_text>>user;
+ 		image_text>>qouta;
+ 		if(user==username){
+ 			image_to_qouta[text_name]=stoi(qouta);
+ 			break;
+ 		}
+ 	}
+
+ 	image_text.close();
+
+
+
+}
+bool Client::Is_Online(string username){
+}
+void Client::Ask_for_qouta(string owner_name,string image_name){
+		vector<string> qouta_p;
+		qouta_p.push_back(this->username);
+		qouta_p.push_back(image_name);
+		rpc_id++;
+		vector<string> qouta_r;
+	if(Is_Online(owner_name)){
+		udpSocket.setPeerAddr(owner_address.first,owner_address.second);
+		
+		Message qouta_request(int(ask_for_qouta),Request,qouta_p,rpc_id);
+		qouta_request.setPeerData(owner_address);
+		Message qouta_reply = execute(qouta_request);
+		qouta_r = qouta_reply.getMessage();
+		if(qouta_r[0]=="true"){image_to_qouta[image_name]=stoi(qouta_r[1]);}
+		else{cout<<"OWNER REFUSED TO GIVE YOU QOUTA";}
+
+
+	}
+	else{
+		Message register_qouta_request(int(ask_for_qouta),Request,qouta_p,rpc_id);
+		Message register_qouta_reply = execute(register_qouta_request);
+		cout<<"Request Sent"<<endl;
+	}
+
+
+
+}
+
+
+
+
+void Client::View_Image(string image_name){
+
+	if(image_to_qouta[image_name]>0){
+		image_to_qouta[image_name]--;
+		//qt function call to show image
+	}
+	else
+	{
+		cout<<"YOU HAVE NO QOUTA"<<endl;
+	}
 }
 Client::~Client(){}
--- Client.h
+++ Client.h
@@ -1,11 +1,12 @@
 #ifndef CLIENT_H
 #define CLIENT_H
+#include <map>
 
 #include "UDPClientSocket.h"
 #include "Message.h"
-
+#include "Image.h"
 //enum MessageType {Request,Reply};
-
+enum operations{login,Take_my_recource,give_me_images_lables,ping,register_request,ask_for_qouta,update_qouta,request_statistics};
 class Client
 {
 private:
@@ -13,8 +14,16 @@ private:
 	char * hostname;
 	int recv_port;
 	int send_port;
+	string username;
+	string password;
+	int rpc_id;
 
+	map<string,string> name_to_image;
+	map<string,int> image_to_qouta;
+	vector<string> images_to_see;
+	
 
+	void ProcessText(string);
 
 	//void CommunicationModule();
 	/*
@@ -42,13 +51,32 @@ private:
 		arquments
 
 	*/
+	
 
 	int getPacketNumber(string message, bool& end);
-
+	pair<string,int>owner_address;
+	string Client_Token;
 
 public:
 	Client(char * _hostname, int _port);
+	Client();
 	Message  execute(Message  _message);
+
+	//Client();
+	//Request Functions From Directory
+	void Take_my_recources(string);
+	void Login(string,string);
+	void Give_me_images();
+	void Ping();
+	bool Is_Online(string username);
+
+	//Request Functions From Peers
+	void Ask_for_qouta(string owner_name,string image_name);
+	void Update_qouta(int new_qouta);
+	void Request_statistics(string image_name);
+	//Frontend Functions
+	void Upload_Image(string image_name);
+	void View_Image(string);
 	~Client();
 };
 //#include "Client.cpp"
